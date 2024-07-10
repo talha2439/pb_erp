1212cf1e5f32b1d2feb0ae77cd2d51575356161c
hassanDurra <hassandurra81@gmail.com>
2024-07-10 20:09:43 +0500

    Added Employee Bank Details In User Profile

--- resources/views/Auth/profile_setting.blade.php
+++ resources/views/Auth/profile_setting.blade.php
@@ -38,23 +38,64 @@
                     </div>
                     <div class="col-lg-6 col-12">
                         <div class="input-block mb-3">
-                            <label>User Name</label>
+                            <label>User Name <small class="text-danger">(*)</small></label>
                             <input type="text" value="{{ $user->username ?? '' }}" name="username" class="form-control"
                                 placeholder="Enter User Name">
                         </div>
                     </div>
                     <div class="col-lg-6 col-12">
                         <div class="input-block mb-3">
-                            <label>Full Name</label>
+                            <label>Full Name <small class="text-danger">(*)</small></label>
                             <input type="text" value="{{ $user->name ?? '' }}" name="name" class="form-control"
                                 placeholder="Enter Full Name">
                         </div>
                     </div>
                     <div class="col-lg-12 col-12">
                         <div class="input-block mb-3">
-                            <label>Email</label>
+                            <label>Email <small class="text-danger">(*)</small></label>
                             <input type="text" value="{{ $user->email ?? '' }}" name="email" class="form-control"
                                 placeholder="Enter Email Address">
+                        </div>
+                    </div><hr>
+                    <div class="col-lg-12">
+                        <div class="form-title">
+                            <h5>Bank Account Information</h5>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-12">
+                        <input type="hidden" name="id" value="{{ $user->employees->id ?? "" }}" id="id">
+                        <div class="input-block mb-3">
+                            <label>Account Holder Name <small class="text-danger">(*)</small></label>
+                            <input type="text" value="{{ $user->employees->bank_details->account_holder_name ?? '' }}" name="username" class="form-control"
+                                placeholder="Enter User Name">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-12">
+                        <div class="input-block mb-3">
+                            <label>Bank Name <small class="text-danger">(*)</small></label>
+                            <input type="text" value="{{ $user->employees->bank_details->name ?? '' }}" name="name" class="form-control"
+                                placeholder="Enter Bank Name">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-12">
+                        <div class="input-block mb-3">
+                            <label>Branch Name <small class="text-danger">(*)</small></label>
+                            <input type="text" value="{{ $user->employees->bank_details->branch_name ?? '' }}" name="name" class="form-control"
+                                placeholder="Enter Branch Name">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-12">
+                        <div class="input-block mb-3">
+                            <label>Account Number <small class="text-danger">(*)</small></label>
+                            <input type="text" value="{{ $user->employees->bank_details->account_number ?? '' }}" name="name" class="form-control"
+                                placeholder="Enter Account Number">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-12">
+                        <div class="input-block mb-3">
+                            <label>IBAN Number</label>
+                            <input type="text" value="{{ $user->employees->bank_details->iban ?? '' }}" name="email" class="form-control"
+                                placeholder="Enter IBAN Number">
                         </div>
                     </div>
 
