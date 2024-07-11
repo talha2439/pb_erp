e49ee29a301bf79512903a6289e7e815724cb89b
hassanDurra <hassandurra81@gmail.com>
2024-07-11 13:18:05 +0500

    Error Solved for Number Currency in Loan

--- resources/views/Admin/employee_loans/index.blade.php
+++ resources/views/Admin/employee_loans/index.blade.php
@@ -73,10 +73,10 @@ Employee Loan List
                             <td>{{  $requested_date->format('F d , Y') }}</td>
                             <td>{{  $due_date->format('F d , Y') }}</td>
                             <td>{{  $requested_date->diffInDays($due_date) .' Days' ?? "0 Days"}}</td>
-                            <td>{{  \Number::currency($item->requested_amount , 'PKR' , 'en_PK') }}</td>
-                            <td>{{  \Number::currency($item->approved_amount  , 'PKR' , 'en_PK') }}</td>
-                            <td>{{  \Number::currency($item->remaining_amount  , 'PKR' , 'en_PK') }}</td>
-                            <td>{{  \Number::currency($item->paid_amount  , 'PKR' , 'en_PK') }}</td>
+                            <td>{{  \Number::currency(!empty($item->requested_amount)  ? $item->requested_amount : 0 , 'PKR' , 'en_PK') : "0" }}</td>
+                            <td>{{  \Number::currency(!empty($item->approved_amount) ? $item->approved_amount : 0  , 'PKR' , 'en_PK') }}</td>
+                            <td>{{  \Number::currency(!empty($item->remaining_amount) ? $item->remaining_amount : 0  , 'PKR' , 'en_PK') }}</td>
+                            <td>{{  \Number::currency(!empty($item->paid_amount) ? $item->paid_amount : 0  , 'PKR' , 'en_PK') }}</td>
                             <td>{{  $item->created_by ?? 'unknown' }}</td>
                             <td>{{ @$item->approved->name  }}</td>
                             <td>{{ @$item->rejected->name }}</td>
