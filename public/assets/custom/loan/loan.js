dc3febc2d6bb1ab3abf1bfdf0051621a591cdf16
hassanDurra <hassandurra81@gmail.com>
2024-07-13 14:28:01 +0500

    Fixed An error in loan managment and Modified it

--- public/assets/custom/loan/loan.js
+++ public/assets/custom/loan/loan.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 
     $("#loanForm").submit(function(e){
         isValid = true;
-        
+
         validate("loanForm",  e);
         if(isValid){
             $("#submitBtn").prop('disabled',true);
@@ -28,6 +28,45 @@ $(document).ready(function(){
             }
         })
     }
-
+    // Repay Type
+    let repayType = $(document).find('#repay_type');
+    $(repayType).on('change', function(e){
+        if($(this).val() == 'monthly'){
+            $('.partial_container').fadeIn();
+            $('.no_months').fadeIn();
+            $('#request_date').attr('data-type','');
+            $('#due_date').attr('data-type','');
+            $('#partial_amount').attr('data-type','required');
+            $('#total_month').attr('data-type','required');
+            $('.partial_container').removeClass('col-md-12');
+            $('.duration_date').hide();
+        }
+        else if ($(this).val() == 'salary'){
+            $('.no_months').fadeIn();
+            $('.partial_container').fadeIn();
+            $('#partial_amount').attr('data-type','required');
+            $('#total_month').attr('data-type','required');
+            $('#request_date').attr('data-type','');
+            $('.duration_date').hide();
+            $('#due_date').attr('data-type','');
+        }
+        else if($(this).val() == 'duration'){
+            $('.partial_container').hide();
+            $('.duration_date').fadeIn();
+            $('.no_months').hide();
+            $('#partial_amount').attr('data-type','');
+            $('#total_month').attr('data-type','');
+            $('#request_date').attr('data-type','required');
+            $('#due_date').attr('data-type','required');
+        }
+    });
+    // Calculate Partial
+    $("#total_month").on('input', function(e){
+        let totalAmount = parseInt($(this).val());
+        let requested_amount = parseInt($("#requested_amount").val());
+        let partialAmount = ((requested_amount / totalAmount) || requested_amount );
+        $("#partial_amount").val(partialAmount);
+    });
+    
 
 })
